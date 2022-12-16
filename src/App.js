bb66e1d28863b8dd29f860f9030b685f5eaf9880
hyunjun06 <junkayden@gmail.com>
2022-12-16 11:42:49 +0900

    styled components #2.5

--- src/App.js
+++ src/App.js
@@ -1,30 +1,56 @@
 import styled from "styled-components";
+import { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 `;
 
-const Button = styled.button`
-  background-color: tomato;
-  border-radius: 15px;
-  border: 0;
+const animation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform:rotate(360deg);
+    border-radius: 0px;
+  }
 `;
 
-const Input = styled.input.attrs({required: true, type: "text"})`
+const Emoji = styled.span`
+  font-size: 36px;
+  color: black;
+`;
+
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
   background-color: tomato;
+  animation: ${animation} 1s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${Emoji} {
+    color: white;
+    &:hover {
+      font-size: 50px;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father>
-      <Button>Click me</Button>
-      <Button as="a">Click me</Button>
-      <Input/>
-      <Input/>
-      <Input/>
-      <Input/>
-      <Input/>
-    </Father>
+    <Wrapper>
+      <Box>
+        <Emoji as="p">test</Emoji>
+      </Box>
+      <Emoji>no hover</Emoji>
+    </Wrapper>
   );
 }
 
