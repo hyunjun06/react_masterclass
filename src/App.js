00be4454b6a6d7df3713eaeea33678e608ef5cca
hyunjun06 <junkayden@gmail.com>
2022-12-16 12:06:12 +0900

    styled componenets #2.7

--- src/App.js
+++ src/App.js
@@ -1,55 +1,22 @@
 import styled from "styled-components";
-import { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
+  background-color: ${(props) => props.theme.backgroundColor};
+  margin: 0;
 `;
 
-const animation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100% {
-    transform:rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-  color: black;
-`;
-
-const Box = styled.div`
-  width: 200px;
-  height: 200px;
-  background-color: tomato;
-  animation: ${animation} 1s linear infinite;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  ${Emoji} {
-    color: white;
-    &:hover {
-      font-size: 50px;
-    }
-  }
-`;
-
+const Title = styled.h1`
+  font-size: 30px;
+  color: ${(props) => props.theme.textColor};
+`
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">test</Emoji>
-      </Box>
-      <Emoji>no hover</Emoji>
+      <Title>This is a title</Title>
     </Wrapper>
   );
 }
